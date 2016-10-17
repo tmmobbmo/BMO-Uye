eb6381a1ae12f1ecf00fabd12ff05dd379f55d9b
Ozgen Muzac <ozgen.muzac@gmail.com>
2016-10-18 02:44:38 +0300

    Routing injected.

--- js/app.js
+++ js/app.js
@@ -1,18 +1,16 @@
-/*global require*/
-'use strict';
-
-require([
-	'angular'
-], function (angular) {
-	require([
-		'controllers/bmo-uye-controller', 
-		'directives/todoFocus', 
-		'directives/todoEscape',
-		'services/todoStorage'
-	], function (bmoUyeController, todoFocusDir, todoEscapeDir, todoStorageSrv) {
-		angular
-			.module('bmoUye', [todoFocusDir, todoEscapeDir, todoStorageSrv])
-			.controller('BmoUyeController', bmoUyeController);
-		angular.bootstrap(document, ['bmoUye']);			
-	});	
-});
+define([
+	'angular',
+	'angular-route',
+	'controllers/bmo-uye-controller', 
+	'directives/todoFocus', 
+	'directives/todoEscape',
+	'services/todoStorage'
+], function(angular, ngRoute, uyeContoller, todoFocusDir, todoEscapeDir, todoStorageSrv){
+	var module = angular.module('bmoUye', ['ngRoute', todoFocusDir, todoEscapeDir, todoStorageSrv])
+		.controller('BmoUyeController', uyeContoller)
+		.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+    		$locationProvider.html5Mode(true);
+    		$locationProvider.hashPrefix('!');
+	}]);
+	angular.bootstrap(document, ['bmoUye'])
+})
--- js/main.js
+++ js/main.js
@@ -3,12 +3,20 @@
 
 require.config({
 	paths: {
-		angular: '../node_modules/angular/angular'
+		'angular': '../node_modules/angular/angular',
+		'ui-router': '../node_modules/angular-ui-router/release/angular-ui-router',
+		'angular-route': '../node_modules/angular-route/angular-route'
 	},
 	shim: {
 		angular: {
 			exports: 'angular'
-		}
+		},
+		'ui-router':{
+            deps: ['angular']
+        },
+        'angular-route':{
+            deps: ['angular']
+        }
 	},
 	deps: ['app']
 });
