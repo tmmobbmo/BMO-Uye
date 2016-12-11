c6fc859f31fa7fc5c2c0f51121666103bf6c1dde
Volkan Tokmak <merhaba@tokmak.org>
2016-12-11 21:45:13 +0300

    root url fix

--- js/services/uye-service.js
+++ js/services/uye-service.js
@@ -8,7 +8,7 @@ define([
         'ngCookies'
     ]).factory('Uye', UyeService);
 
-    var rootUrl = 'http://192.168.1.100:8087';
+    var rootUrl = '';
 
     function UyeService($http, $q, $cookies, $interpolate) {
         var service = {
